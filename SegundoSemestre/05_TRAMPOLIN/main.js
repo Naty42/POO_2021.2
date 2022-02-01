f1ff9e3cf62724251037eb5049777b11c9b4ee50
Nataly Campelo <84623542+Naty42@users.noreply.github.com>
2022-02-01 09:29:28 -0300

    Delete SegundoSemestre/05_TRAMPOLIN directory

--- SegundoSemestre/05_TRAMPOLIN/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var io_1 = require("./io");
-function main() {
-    var nome = io_1.io.input("seu nome: ");
-    io_1.io.print("olá " + nome, '');
-    io_1.io.print(", como você está hoje??");
-}
-main();
